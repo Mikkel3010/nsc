a9ad9116e02cf142d9877c252a572f11459d4a10
Mikkel3010 <mikkel30101234@live.dk>
2026-02-05 13:41:26 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import numpy
+import matplotlib
+import scipy
+import numba
+import pytest
+import dask
+
+print(numpy.__version__)
+print(matplotlib.__version__)
+print(scipy.__version__)
+print(numba.__version__)
+print(pytest.__version__)
+print(dask.__version__)
